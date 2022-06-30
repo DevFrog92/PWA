258fadac4fe9baa47a913f9fb8574d6bbdf55424
DevFrog92 <yongsu1481@gmail.com>
2022-06-30 22:27:05 +0900

    ✨ Create vue pwa project and connect with firebase for deploy

---
+++ vue-pwa/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <h1 class="title white--text">반가원요!</h1>
+            <p class="caption">by VueJS</p>
+            <img src="./assets/profile.jpeg" width="300" alt="profile image" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  created() {
+    // 다크 모드를 사용
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
